83c02b59c12d971c0c6df2d977f9f957d748200b
Tobias Pickel <tobias.pickel@xing.com>
2018-12-19 15:39:39 +0100

    refactor(app): use global styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+/** @jsx jsx */
+
 import { ArticleList } from './article/article-list';
-import React, { Component } from 'react';
+import { Component } from 'react';
 import {
   ThemeProvider,
   light,
@@ -7,14 +9,39 @@ import {
   HeaderNavBar,
   ThemeSwitcher,
   Block,
+  Global,
+  css,
+  jsx,
 } from 'bricks-of-sand';
-
-import './app.css';
 
 class App extends Component {
   render() {
     return (
       <ThemeProvider themes={{ light, dark }}>
+        <Global
+          styles={css`
+            html {
+              font-size: 18px;
+              font-family: sans-serif;
+            }
+
+            label {
+              display: block;
+            }
+
+            @media only screen and (min-width: 30em) {
+              html {
+                font-size: 24px;
+              }
+            }
+            h1,
+            h2,
+            h3 {
+              text-transform: uppercase;
+              font-weight: lighter;
+            }
+          `}
+        />
         <HeaderNavBar>
           <Block margin="1rem">
             <ThemeSwitcher />
