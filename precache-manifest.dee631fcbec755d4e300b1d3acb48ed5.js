4bba7db210a7586fa622428d306e9eea52d64c14
Tobias Pickel <tobias.pickel@xing.com>
2019-04-25 10:12:25 +0200

    Updates

---
+++ precache-manifest.dee631fcbec755d4e300b1d3acb48ed5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac297154c5a704f4f4219408d4de64f",
+    "url": "/hooks-list/index.html"
+  },
+  {
+    "revision": "2f2b95d11494878fa5cb",
+    "url": "/hooks-list/static/js/2.90cfb3e9.chunk.js"
+  },
+  {
+    "revision": "3c5ea8507e5ac58b5c2a",
+    "url": "/hooks-list/static/js/main.4f94794b.chunk.js"
+  },
+  {
+    "revision": "b7088e3be42b2dc4bf87",
+    "url": "/hooks-list/static/js/runtime~main.ea5711c8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/hooks-list/precache-manifest.7c20cdc3571aab1bedffcea158704f66.js"
+  "/hooks-list/precache-manifest.dee631fcbec755d4e300b1d3acb48ed5.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/hooks-list/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hooks-list/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
